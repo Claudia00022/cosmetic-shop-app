dd134a6d1024afa037eef338b0e82251a4080354
Claudia00022 <klaudiaforysiak@gmail.com>
2024-03-06 18:25:34 +0000

    navbar prettier

--- src/components/Navbar/CollapseNavbar.jsx
+++ src/components/Navbar/CollapseNavbar.jsx
@@ -1,99 +1,51 @@
-// import React, {useState} from "react";
-// import { NavLink } from "react-router-dom";
 
-// function Navbar() {
-
-//    const[expanded, setExpanded] = useState(false);
-
-//   return (
-//     <div>
-//       <div className="container fixed-top">
-//         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-//           <a class="navbar-brand" href="/">
-//             logo
-//           </a>
-//           <button
-//             class="navbar-toggler"
-//             type="button"
-//             data-toggle="collapse"
-//             data-target="#navbarNavAltMarkup"
-//             aria-controls="navbarNavAltMarkup"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-
-//              onClick={() => setExpanded(!expanded)}
-//           >
-//             <span class="navbar-toggler-icon"></span>
-//           </button>
-
-//           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-//             <div class="navbar-nav">
-//               <NavLink to="/skinCare" className ="nav-item nav-link">
-//                 skin care
-//               </NavLink>
-//               <NavLink to="/bodyCare" className ="nav-item nav-link">
-//                 body care
-//               </NavLink>
-//               <NavLink to="/hairCare" className ="nav-item nav-link">
-//                 hair care
-//               </NavLink>
-
-//               <a class="nav-item nav-link" href="#">
-//                 Log In
-//               </a>
-//               <a class="nav-item nav-link" href="#">
-//                 Img cart
-//               </a>
-//             </div>
-//           </div>
-//         </nav>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
-
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpa } from "@fortawesome/free-solid-svg-icons";
+
+const logo =  <FontAwesomeIcon icon={faSpa} size="lg" style={{color: "#ffffff",}} />;
 
 function CollapseNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="navigation-home">
       <div className=" container-fluid">
-        <Navbar.Brand className="me-5 text-white" href="#">
-          logo
+        <Navbar.Brand className="me-5 text-white logo" href="#">
+          logo <span className="ms-3">{logo}</span>
         </Navbar.Brand>
+        <div className="right-border"></div>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="w-100">
-            <div className="d-flex">
+            <div className="d-flex align-items-center ">
               <NavLink
                 to="skinCare"
-                className="nav-item nav-link me-5 text-white "
+                className="nav-item nav-link  text-white navbar-item "
               >
                 skin care
               </NavLink>
+              <div className="right-border"></div>
               <NavLink
                 to="bodyCare"
-                className="nav-item nav-link me-5 text-white"
+                className="nav-item nav-link text-light  navbar-item "
               >
                 body care
               </NavLink>
+              <div className="right-border"></div>
               <NavLink
                 to="HairCare"
-                className="nav-item nav-link me-5 text-white"
+                className="nav-item nav-link  text-white navbar-item "
               >
                 hair care
               </NavLink>
+              <div className="right-border"></div>
             </div>
-            <div className="d-flex ms-md-auto ">
-              <button className="btn btn-primary">
+            <div className="d-flex ms-md-auto align-items-center ">
+              <button className="logIn-btn">
                 <NavLink
                   to="HairCare"
-                  className="nav-item nav-link text-white "
+                  className="nav-item nav-link text-white  "
                 >
                   Log In
                 </NavLink>
@@ -102,7 +54,7 @@ function CollapseNavbar() {
                 {" "}
                 <NavLink
                   to="HairCare"
-                  className="nav-item nav-link me-5 text-white"
+                  className="nav-item nav-link text-white ms-3 me-5 fw-light"
                 >
                   <span class="material-symbols-outlined">shopping_cart</span>
                 </NavLink>
