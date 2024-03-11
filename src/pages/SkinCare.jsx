050a54995d9d856f09902de56a5e7673fa020e00
Claudia00022 <klaudiaforysiak@gmail.com>
2024-03-11 18:00:59 +0000

    accordion added

--- src/pages/SkinCare.jsx
+++ src/pages/SkinCare.jsx
@@ -1,17 +1,63 @@
 import React from "react";
 import CosmeticCard from "../components/Navbar/Card";
+import Accordion from 'react-bootstrap/Accordion';
+
 
 function SkinCare(){
     return <>
        <div className="d-flex bg-white height ">
-       <div className="w-50"></div>
-       <div className="w-50 row">
-        <div className="col-6"><CosmeticCard /></div>
-        <div className="col-6"><CosmeticCard /></div>
-        <div className="col-6"><CosmeticCard /></div>
-        <div className="col-6"><CosmeticCard /></div>
-        <div className="col-6"><CosmeticCard /></div>
-        <div className="col-6"><CosmeticCard /></div>
+       
+       <div className="left-acc ms-3 border-end fixed-top">
+        <p className="ms-5">skin care</p>
+        <hr></hr>
+        <div>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0" className="border-0">
+        <Accordion.Header>type of skin </Accordion.Header>
+        <Accordion.Body>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1" className="border-0">
+        <Accordion.Header>products </Accordion.Header>
+        <Accordion.Body>
+        <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2" className="border-0">
+        <Accordion.Header>ingredients</Accordion.Header>
+        <Accordion.Body>
+        <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+          <p>lorem ipsum</p>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+        </div>
+       </div>
+       <div className="w-75 ms-auto  row">
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
+        <div className="col-4"><CosmeticCard /></div>
         
         </div>
 
